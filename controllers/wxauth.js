f74b6d7abda3b45350160e35ec22f2ad901b050f
zflan <lanzf2011@gmail.com>
2016-11-23 20:33:09 +0800

    入参处理

--- controllers/wxauth.js
+++ controllers/wxauth.js
@@ -7,8 +7,9 @@ const WXBizDataCrypt = require('../libs/WXBizDataCrypt');
 const GET_SESSION_KEY_URL = "https://api.weixin.qq.com/sns/jscode2session?appid=wxd95c962a6b7690df&secret=d7b80d870a7aa764f2114484a89f295c&grant_type=authorization_code&js_code=";
 
 
-module.exports.getSession_key = function* getSession_key(code) {
-
+module.exports.getSession_key = function* getSession_key(cxt) {
+	var code = cxt.req.code;
+	console.log("------get_session_key  code-------" + code);
 	co(function*() {
 		var url = GET_SESSION_KEY_URL + code;
 		console.log("------get_session_key_url-------" + url);
