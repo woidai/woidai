9ac836f00a7db9a15490d7bc742d1bd43ce1f101
zflan <lanzf2011@gmail.com>
2016-12-12 16:15:47 +0800

    修改bug,数据版本表初始化

--- services/service_update.js
+++ services/service_update.js
@@ -1,13 +1,25 @@
 var service = require('./dbservice');
 var mongoStore = require('koa-session-mongo');
 var ObjectID = require('mongodb').ObjectID;
+const INIT_SQL = {
+					table : "region",
+					version : "0"
+				};
 
 var Service = function() {
 	this.collectionName = 'version';
 
 	//查找某个表的版本
 	this.findDataVersion = function*(table) {
-		var res = yield this.findOne({table: table});
+		var isExist = yield this.isExist();
+		var res;
+		if (isExist) {
+			res = yield this.findOne({table: table});
+		}else{
+			yield this.init();
+			res = {table: table, version:0};
+		}
+
 		return res;
 	}
 
@@ -20,6 +32,11 @@ var Service = function() {
 		res.updateDate = new Date();
 		var result = yield this.update(res);
 		return result;
+	}
+
+	this.init = function *(){
+		console.log("初始化数据表");
+		yield this.insert(INIT_SQL);
 	}
 
 	if (Service.instance == null) {
